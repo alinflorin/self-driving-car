08ab72ecba8ecbba13f0e06fcf5d3f1ef9b10be0
alinflorin <alin@qoffice.ro>
2022-05-19 00:13:49 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -9,8 +9,8 @@ const ctx = canvas.getContext('2d');
 const car = new Car(100, 100, 30, 50);
 
 const animate = () => {
-  car.draw(ctx);
   canvas.height = window.innerHeight;
+  car.draw(ctx);
   requestAnimationFrame(animate);
 };
 
